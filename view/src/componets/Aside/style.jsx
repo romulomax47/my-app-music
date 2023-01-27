fa4ff437d6b4ac59fa4017ac65d4ce3cb5192de0
romulo max <maxromulo16@gmail.com>
2023-01-27 20:36:36 -0300

    Style: style nos buttons

--- view/src/componets/Aside/style.jsx
+++ view/src/componets/Aside/style.jsx
@@ -1,11 +1,77 @@
 
 import styled from "styled-components";
-
+import {BiHomeAlt, BiSearchAlt} from 'react-icons/bi';
 
 export const Container = styled.div`
 
    grid-area: asid;
    height: 100%;
-   border: 1px solid black;
+   width: 100%;
 
+   background-color: var(--primary);
+   color: var(--white);
+
+   padding: 20px 15px;
+
+`
+
+export const Header = styled.div`
+   width: 100%;
+   text-align: center;
+   
+   margin-bottom: 20px;
+
+`
+
+export const Home = styled.div`
+   width: 100%;
+   background-color: var(--segundary);
+
+   height: 40px;
+   padding: 15px 20px;
+   border-radius: 10px;
+   display: flex;
+   align-items: center;
+   gap: 15px;
+
+   margin-bottom: 20px;
+   cursor: pointer;
+
+   >span{
+      color: var(--white);
+      text-decoration: none;
+      font-size: 25px;
+      font-weight: bolder;
+   }
+ 
+`
+
+export const HomeIcon = styled(BiHomeAlt)`
+   font-size: 25px;
+`
+
+export const Search = styled.div`
+
+    width: 100%;
+   background-color: var(--segundary);
+
+   height: 40px;
+   padding: 15px 20px;
+   border-radius: 10px;
+   display: flex;
+   align-items: center;
+   gap: 15px;
+   cursor: pointer;
+
+   >span{
+      color: var(--white);
+      text-decoration: none;
+      font-size: 25px;
+      font-weight: bolder;
+   }
+
+
+`
+export const SearchIcon = styled(BiSearchAlt)`
+     font-size: 25px;
 `
